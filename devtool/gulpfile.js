414fb1d154da16e3f8e7b44b7ea6c1bb9cf690fd
大坪 俊平 <ootsuboshunpei@ootsubo-no-iMac.local>
2019-11-13 01:03:58 +0900

    Add files

---
+++ devtool/gulpfile.js
@@ -0,0 +1,52 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const plumber = require("gulp-plumber");
+const sourcemaps = require("gulp-sourcemaps");
+const progeny = require("gulp-progeny"); // cssの差分ビルドのため
+const browserSync = require("browser-sync").create();
+
+
+gulp.task("sass", function() {
+  return gulp
+    .src("../html/src/scss/bundle.scss")
+    .pipe(plumber())
+    .pipe(progeny())
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        outputStyle: "expanded"
+      })
+    )
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("../html/dist/css/"))
+});
+
+
+gulp.task("server", function(done) {
+  browserSync.init({
+    server: {
+      // サーバの立ち上がる位置
+      baseDir: "../",
+      index: "/html/index.php"
+    }
+  });
+  done();
+  console.log("+------------------+ server was launched +------------------+");
+});
+
+
+gulp.task("watch", function(done) {
+  gulp.watch("../html/src/scss/*.scss", gulp.task("sass"));
+  gulp.watch("../html/dist/css/*.css", gulp.task("server"));
+  gulp.watch("../html/*/*.html", gulp.task("server"));
+  gulp.watch("../html/dist/js/*.js", gulp.task("server"));
+  gulp.watch("../")
+  .on("change", function() {
+    browserSync.reload();
+  });
+  done();
+  console.log("+------------------+ Gulp watch start +------------------+");
+});
+
+
+gulp.task("default", gulp.parallel("server", "watch"));
---
+++ doc/blueprint.md
@@ -0,0 +1,99 @@
+# 構成図
+
+## アプリのゴール
+
+**ファーストステップ**
+- メンバー登録アプリを作成する
+  - セキュリティを考慮する
+  - テストコード対応
+  - パブリック公開
+
+**セカンドステップ**
+- メンバー登録アプリを掲示板アプリにする
+
+**サードステップ**
+- 管理画面を作る
+
+---
+
+## DB
+
+### ファーストステップ
+メンバー登録アプリ
+
+```
+mst_member
+- id(PK)
+- name
+- code
+- pass
+- created_at
+- updated_at
+- deleted_at
+```
+
+### セカンドステップ
+掲示板アプリ
+
+```
+mst_board
+- id(PK)
+- mem_name
+- mem_code
+- post
+- created_at
+- updated_at
+- deleted_at
+```
+
+```
+mst_posts
+- id(PK)
+- mem_code
+- posts
+- created_at
+- updated_at
+- deleted_at
+```
+
+### サードステップ
+管理画面
+
+```
+mst_admin
+- id(PK)
+- admin_name
+- admin_mem_code
+- created_at
+- updated_at
+- deleted_at
+```
+
+```
+history_edit
+- id(PK)
+- edit_name
+- edit_board_id
+- edit_board_detail
+- edit_mem_name
+- edit_mem_pass
+- delete_board_id
+- delete_mem_name
+- delete_mem_code
+- created_at
+- updated_at
+- deleted_at
+```
+
+---
+
+## スケジュール
+
+- ~11/13 ... 画面遷移設計完了 0%
+- ~11/16 ... メンバー登録アプリ作成完了 0%
+- ~11/18 ... 掲示板アプリ画面遷移設計完了 0%
+- ~11/21 ... 掲示板アプリ作成完了 0%
+- ~11/23 ... 管理画面遷移設計完了 0%
+- ~11/27 ... 管理画面追加完了 0%
+- ~11/30 ... テスト完了 0%
+- ~12/4 ... デプロイ 0%
